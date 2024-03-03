82dda7fa3d0e0af113ef1de2653730c788f9497c
AlekAz3 <vakarin2020@gmail.com>
2024-03-03 13:57:18 +0300

    Первые попытки

---
+++ Cipher.Logic/CharExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Cipher.Logic;
+
+public static class CharExtensions
+{
+    public static char JumpLetter(this char letter, int a)
+    {
+        var position = Convert.ToInt32(letter);
+
+        if (position is >= 65 and <= 90)
+        {
+            position += a;
+            if (position <= 65 )
+            {
+                position += 26;
+            }
+            else if (position>=90)
+            {
+                position -= 26;
+            }
+        }
+
+        if (position is >= 97 and <= 122)
+        {
+            position += a;
+            if (position <= 97 )
+            {
+                position += 26;
+            }
+            else if (position>=122)
+            {
+                position -= 26;
+            }
+        }
+
+        return Convert.ToChar(position);
+    }
+}
---
+++ Cipher.Tests/ExtensionsTests/CharExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using Cipher.Logic;
+
+namespace Cipher.Tests.ExtensionsTests;
+
+public class CharExtensionsTests
+{
+    [Theory]
+    [InlineData('a', 'b')]
+    public void Test1(char a, char b)
+    {
+        var c = a.JumpLetter(1);
+        Assert.Equal(b, c);
+    }
+}
