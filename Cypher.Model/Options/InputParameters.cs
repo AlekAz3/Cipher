fff099692a0ea1ccf0b17af37ffff801f4197b6c
AlekAz3 <vakarin2020@gmail.com>
2024-03-03 13:12:29 +0300

    45

--- Cypher.Model/Options/InputParameters.cs
+++ Cypher.Model/Options/InputParameters.cs
@@ -2,6 +2,13 @@
 
 public class InputParameters
 {
+    /// <summary>
+    /// Исходный текст
+    /// </summary>
     public string? InputText { get; set; }
+
+    /// <summary>
+    /// Ключ
+    /// </summary>
     public string? Key { get; set; }
 }
